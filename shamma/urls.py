4eb64b2e8657d378df706335a875c2357d17f02d
Hossein Sekhavat <sekhavat17@gmail.com>
2015-02-03 09:57:54 +0330

    Include urls for subsystems

--- shamma/urls.py
+++ shamma/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+import references.urls
+import accounts.urls
+import categories.urls
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'shamma.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='homepage'),
+    url(r'^references/', include(references.urls)),
+    url(r'^accounts/', include(accounts.urls)),
+    url(r'^categories/', include(categories.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
