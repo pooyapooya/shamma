f996b84e2aae16dfb6863a1ace66c0c5bc5f7c48
po0ya <pooya.mosaddegh72@gmail.com>
2015-02-09 05:48:40 +0000

    create categories test and run

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=20)
+    parent = models.ForeignKey('categories.Topic', related_name='topics', null=True, blank=True)
--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from categories.models import Topic
+
+
+class TopicCreateTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def topic_can_create(self):
+        Topic.objects.create(name='Agile', parent=None)
+        self.assert_(True, 'Agile topic created successfully')
+        
