7988a1707480b022c4784e2c60166b35098ae2d2
Hossein Sekhavat <sekhavat17@gmail.com>
2015-02-14 04:37:02 +0330

    updatae reqs

--- references/forms.py
+++ references/forms.py
@@ -10,11 +10,6 @@ class CreateReferenceForm(forms.ModelForm):
 
     reference_type = forms.ChoiceField(choices=Reference.get_type_choices())
 
-    def clean_reference_type(self):
-        data = self.cleaned_data['reference_type']
-        print self.instance
-        return data
-
     def save(self, commit=True):
         ret = super(CreateReferenceForm, self).save(commit=False)
         if commit:
